97015c69d2361c85ab10282ff1f6549f8b246c2d
Kozo Komiya <kozo.komiya@gmail.com>
2018-10-17 17:43:02 +0900

    Initial Commit

---
+++ EquirectangularConversion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EquirectangularConversion
+//
+//  Created by Kozo Komiya on 2018/10/17.
+//  Copyright © 2018 Kozo Komiya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
